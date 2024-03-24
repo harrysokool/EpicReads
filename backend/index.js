8e021b6d165df8147b42dfae06e9e1f7208003fd
“harrysokool” <harry1232003@icloud.com>
2024-03-23 18:52:33 -0700

    set up mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,33 @@
 import express from 'express';
 // use port in this file
-import { PORT } from './config.js'
+import { PORT, MONGODB_URL } from './config.js'
+import mongoose from 'mongoose';
 
 const app = express();
 
 // create a route for the server, / is the root route, and we have a callback function
-// get is a method, and it gets resources
+// get is a http method.
 app.get('/', (request, response) => {
-    response.send('Hello World');
-    console.log('Hello World');
+    console.log(request);
     return response.status(234).send("testing");
 })
 
 // create a function for listening to this port with a callback function
 // in terminal, type in "npm run dev", this will run the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// connect to the database
+// we want to run the express server after we connect to the database
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log('Connected to the database');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
