2432d706bee727c6442315026b492eae249fb110
“harrysokool” <harry1232003@icloud.com>
2024-03-23 18:36:47 -0700

    added a http requet: get

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,16 @@ import { PORT } from './config.js'
 
 const app = express();
 
+// create a route for the server, / is the root route, and we have a callback function
+// get is a method, and it gets resources
+app.get('/', (request, response) => {
+    response.send('Hello World');
+    console.log('Hello World');
+    return response.status(234).send("testing");
+})
+
 // create a function for listening to this port with a callback function
+// in terminal, type in "npm run dev", this will run the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
