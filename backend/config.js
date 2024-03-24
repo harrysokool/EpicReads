d2ae615370efe25e63f08ed945d7a81a87b9bb83
“harrysokool” <harry1232003@icloud.com>
2024-03-23 19:11:52 -0700

    created model with mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 // export so other class can use it too!
 export const PORT = 5555; 
 
-export const MONGODB_URL = 'mongodb+srv://harry707:Carol123.@book-store-mern.bi3fksx.mongodb.net/?retryWrites=true&w=majority&appName=book-store-MERN';
+export const MONGODB_URL = 'mongodb+srv://harry707:Carol123.@book-store-mern.bi3fksx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store-MERN';
 
---
+++ backend/models/bookModel.js
@@ -0,0 +1,11 @@
+// this file keeps all the project model
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  publishYear:  { type: Number, required: true },
+  timestamps : true // this will automatically create a timestamp for the book Mongoose will    automatically add createdAt and updatedAt fields to the documents based on the timestamps of document creation and last update
+});
+
+export const book = mongoose.model('Kitten', bookSchema);
